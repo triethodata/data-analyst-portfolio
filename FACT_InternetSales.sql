43f82a503e46b874be47dcf0333f3c37f20b5f68
triethodata <128765957+triethodata@users.noreply.github.com>
2023-03-24 12:55:36 +0700

    Add files via upload

---
+++ FACT_InternetSales.sql
@@ -0,0 +1,15 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -4
+ORDER BY
+  OrderDateKey ASC
